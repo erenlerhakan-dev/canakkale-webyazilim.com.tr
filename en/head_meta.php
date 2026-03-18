dc43e136a62a8134dcac5421e0cec4a8850b4b5c
hakan <hakan@hakan>
2026-03-18 16:25:41 +0300

    start 2026

--- en/head_meta.php
+++ en/head_meta.php
@@ -2,7 +2,7 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<link rel="icon" href="/assets/images/favicon.webp">
+<link rel="icon" href="/images/favicon.webp">
 <link rel="stylesheet" href="/assets/css/vendor/bootstrap.min.css">
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
