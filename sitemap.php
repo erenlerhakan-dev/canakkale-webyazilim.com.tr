f3e815e08df7bd3e2a6db0ca43b4df76713d1c7c
hakan <hakan@hakan>
2026-03-19 01:10:24 +0300

    start 2026

--- sitemap.php
+++ sitemap.php
@@ -19,7 +19,7 @@ echo '
 
 echo '
 <url>
-<loc>'; echo "https://www.canakkale-webyazilim.com.tr"; echo 'dovme-hakkinda-sik-sorulan-sorular.php</loc>
+<loc>'; echo "https://www.canakkale-webyazilim.com.tr/"; echo 'hakkimizda</loc>
 <lastmod>'; echo date("m"); echo '</lastmod>
 <changefreq>weekly</changefreq>
 <priority>1.0</priority>
@@ -28,7 +28,7 @@ echo '
 
 echo '
 <url>
-<loc>'; echo "https://www.canakkale-webyazilim.com.tr"; echo 'hizmetler/index.php</loc>
+<loc>'; echo "https://www.canakkale-webyazilim.com.tr/"; echo 'iletisim</loc>
 <lastmod>'; echo date("m"); echo '</lastmod>
 <changefreq>weekly</changefreq>
 <priority>1.0</priority>
@@ -37,7 +37,385 @@ echo '
 
 echo '
 <url>
-<loc>'; echo "https://www.canakkale-webyazilim.com.tr"; echo 'blog/index.php</loc>
+<loc>'; echo "https://www.canakkale-webyazilim.com.tr/"; echo 'hizmetler</loc>
+<lastmod>'; echo date("m"); echo '</lastmod>
+<changefreq>weekly</changefreq>
+<priority>1.0</priority>
+</url>
+';
+
+echo '
+<url>
+<loc>'; echo "https://www.canakkale-webyazilim.com.tr/hizmetlerimiz/"; echo 'api-gelistirme</loc>
+<lastmod>'; echo date("m"); echo '</lastmod>
+<changefreq>weekly</changefreq>
+<priority>1.0</priority>
+</url>
+';
+
+echo '
+<url>
+<loc>'; echo "https://www.canakkale-webyazilim.com.tr/hizmetlerimiz/"; echo 'b2b-ve-b2c-cozumleri</loc>
+<lastmod>'; echo date("m"); echo '</lastmod>
+<changefreq>weekly</changefreq>
+<priority>1.0</priority>
+</url>
+';
+
+echo '
+<url>
+<loc>'; echo "https://www.canakkale-webyazilim.com.tr/hizmetlerimiz/"; echo 'crm-sistemleri</loc>
+<lastmod>'; echo date("m"); echo '</lastmod>
+<changefreq>weekly</changefreq>
+<priority>1.0</priority>
+</url>
+';
+
+echo '
+<url>
+<loc>'; echo "https://www.canakkale-webyazilim.com.tr/hizmetlerimiz/"; echo 'e-ticaret-cozumleri</loc>
+<lastmod>'; echo date("m"); echo '</lastmod>
+<changefreq>weekly</changefreq>
+<priority>1.0</priority>
+</url>
+';
+
+echo '
+<url>
+<loc>'; echo "https://www.canakkale-webyazilim.com.tr/hizmetlerimiz/"; echo 'erp-cozumleri</loc>
+<lastmod>'; echo date("m"); echo '</lastmod>
+<changefreq>weekly</changefreq>
+<priority>1.0</priority>
+</url>
+';
+
+echo '
+<url>
+<loc>'; echo "https://www.canakkale-webyazilim.com.tr/hizmetlerimiz/"; echo 'mobil-uygulamalar</loc>
+<lastmod>'; echo date("m"); echo '</lastmod>
+<changefreq>weekly</changefreq>
+<priority>1.0</priority>
+</url>
+';
+
+echo '
+<url>
+<loc>'; echo "https://www.canakkale-webyazilim.com.tr/hizmetlerimiz/"; echo 'ozel-web-gelistirme</loc>
+<lastmod>'; echo date("m"); echo '</lastmod>
+<changefreq>weekly</changefreq>
+<priority>1.0</priority>
+</url>
+';
+
+echo '
+<url>
+<loc>'; echo "https://www.canakkale-webyazilim.com.tr/hizmetlerimiz/"; echo 'ozel-yazilim-gelistirme</loc>
+<lastmod>'; echo date("m"); echo '</lastmod>
+<changefreq>weekly</changefreq>
+<priority>1.0</priority>
+</url>
+';
+
+echo '
+<url>
+<loc>'; echo "https://www.canakkale-webyazilim.com.tr/hizmetlerimiz/"; echo 'rezervasyon-sistemleri</loc>
+<lastmod>'; echo date("m"); echo '</lastmod>
+<changefreq>weekly</changefreq>
+<priority>1.0</priority>
+</url>
+';
+
+echo '
+<url>
+<loc>'; echo "https://www.canakkale-webyazilim.com.tr/hizmetlerimiz/"; echo 'sunucu-guvenligi-ve-bakimi</loc>
+<lastmod>'; echo date("m"); echo '</lastmod>
+<changefreq>weekly</changefreq>
+<priority>1.0</priority>
+</url>
+';
+
+echo '
+<url>
+<loc>'; echo "https://www.canakkale-webyazilim.com.tr/hizmetlerimiz/"; echo 'teknik-danismanlik</loc>
+<lastmod>'; echo date("m"); echo '</lastmod>
+<changefreq>weekly</changefreq>
+<priority>1.0</priority>
+</url>
+';
+
+echo '
+<url>
+<loc>'; echo "https://www.canakkale-webyazilim.com.tr/hizmetlerimiz/"; echo 'yazilim-performans-optimizasyonu</loc>
+<lastmod>'; echo date("m"); echo '</lastmod>
+<changefreq>weekly</changefreq>
+<priority>1.0</priority>
+</url>
+';
+
+echo '
+<url>
+<loc>'; echo "https://www.canakkale-webyazilim.com.tr/blog/"; echo '2026-icin-gelismis-seo-optimizasyon-stratejileri</loc>
+<lastmod>'; echo date("m"); echo '</lastmod>
+<changefreq>weekly</changefreq>
+<priority>1.0</priority>
+</url>
+';
+
+echo '
+<url>
+<loc>'; echo "https://www.canakkale-webyazilim.com.tr/blog/"; echo '2026da-modern-web-gelistirme-trendleri</loc>
+<lastmod>'; echo date("m"); echo '</lastmod>
+<changefreq>weekly</changefreq>
+<priority>1.0</priority>
+</url>
+';
+
+echo '
+<url>
+<loc>'; echo "https://www.canakkale-webyazilim.com.tr/blog/"; echo 'b2b-platform-gelistirme-ozellikler-ve-en-iyi-uygulamalar</loc>
+<lastmod>'; echo date("m"); echo '</lastmod>
+<changefreq>weekly</changefreq>
+<priority>1.0</priority>
+</url>
+';
+
+echo '
+<url>
+<loc>'; echo "https://www.canakkale-webyazilim.com.tr/blog/"; echo 'bulut-hosting-cozumlerine-eksiksiz-rehber</loc>
+<lastmod>'; echo date("m"); echo '</lastmod>
+<changefreq>weekly</changefreq>
+<priority>1.0</priority>
+</url>
+';
+
+echo '
+<url>
+<loc>'; echo "https://www.canakkale-webyazilim.com.tr/blog/"; echo 'buyume-icin-veri-analitigi-ve-is-zekasi</loc>
+<lastmod>'; echo date("m"); echo '</lastmod>
+<changefreq>weekly</changefreq>
+<priority>1.0</priority>
+</url>
+';
+
+echo '
+<url>
+<loc>'; echo "https://www.canakkale-webyazilim.com.tr/blog/"; echo 'cevik-yazilim-gelistirme-en-iyi-uygulamalar-ve-metodolojiler</loc>
+<lastmod>'; echo date("m"); echo '</lastmod>
+<changefreq>weekly</changefreq>
+<priority>1.0</priority>
+</url>
+';
+
+echo '
+<url>
+<loc>'; echo "https://www.canakkale-webyazilim.com.tr/blog/"; echo 'e-ticaret-platformlari-icin-odeme-arayuzu-entegrasyonu</loc>
+<lastmod>'; echo date("m"); echo '</lastmod>
+<changefreq>weekly</changefreq>
+<priority>1.0</priority>
+</url>
+';
+
+echo '
+<url>
+<loc>'; echo "https://www.canakkale-webyazilim.com.tr/blog/"; echo 'erp-sistemi-uygulamasi-kapsamli-rehber</loc>
+<lastmod>'; echo date("m"); echo '</lastmod>
+<changefreq>weekly</changefreq>
+<priority>1.0</priority>
+</url>
+';
+
+echo '
+<url>
+<loc>'; echo "https://www.canakkale-webyazilim.com.tr/blog/"; echo 'hizmet-isletmeleri-icin-ozel-rezervasyon-motorlari-gelistirme</loc>
+<lastmod>'; echo date("m"); echo '</lastmod>
+<changefreq>weekly</changefreq>
+<priority>1.0</priority>
+</url>
+';
+
+echo '
+<url>
+<loc>'; echo "https://www.canakkale-webyazilim.com.tr/blog/"; echo 'ilerici-web-uygulamalari-web-gelistirmenin-gelecegi</loc>
+<lastmod>'; echo date("m"); echo '</lastmod>
+<changefreq>weekly</changefreq>
+<priority>1.0</priority>
+</url>
+';
+
+echo '
+<url>
+<loc>'; echo "https://www.canakkale-webyazilim.com.tr/blog/"; echo 'is-uygulamalari-icin-blockchain-teknolojisi</loc>
+<lastmod>'; echo date("m"); echo '</lastmod>
+<changefreq>weekly</changefreq>
+<priority>1.0</priority>
+</url>
+';
+
+echo '
+<url>
+<loc>'; echo "https://www.canakkale-webyazilim.com.tr/blog/"; echo 'is-uygulamalari-icin-yapay-zeka-ve-makine-ogrenimi</loc>
+<lastmod>'; echo date("m"); echo '</lastmod>
+<changefreq>weekly</changefreq>
+<priority>1.0</priority>
+</url>
+';
+
+echo '
+<url>
+<loc>'; echo "https://www.canakkale-webyazilim.com.tr/blog/"; echo 'isletmeniz-icin-dogru-cms-platformunu-secmek</loc>
+<lastmod>'; echo date("m"); echo '</lastmod>
+<changefreq>weekly</changefreq>
+<priority>1.0</priority>
+</url>
+';
+
+echo '
+<url>
+<loc>'; echo "https://www.canakkale-webyazilim.com.tr/blog/"; echo 'javascript-framework-karsilastirmasi-react-vue-angular-svelte</loc>
+<lastmod>'; echo date("m"); echo '</lastmod>
+<changefreq>weekly</changefreq>
+<priority>1.0</priority>
+</url>
+';
+
+echo '
+<url>
+<loc>'; echo "https://www.canakkale-webyazilim.com.tr/blog/"; echo 'kucuk-isletmeler-icin-e-ticaret-basari-stratejileri</loc>
+<lastmod>'; echo date("m"); echo '</lastmod>
+<changefreq>weekly</changefreq>
+<priority>1.0</priority>
+</url>
+';
+
+echo '
+<url>
+<loc>'; echo "https://www.canakkale-webyazilim.com.tr/blog/"; echo 'kullanici-deneyimini-sekillendiren-ui-ux-tasarim-trendleri</loc>
+<lastmod>'; echo date("m"); echo '</lastmod>
+<changefreq>weekly</changefreq>
+<priority>1.0</priority>
+</url>
+';
+
+echo '
+<url>
+<loc>'; echo "https://www.canakkale-webyazilim.com.tr/blog/"; echo 'laravel-neden-kurumsal-uygulamalar-icin-mukemmeldir</loc>
+<lastmod>'; echo date("m"); echo '</lastmod>
+<changefreq>weekly</changefreq>
+<priority>1.0</priority>
+</url>
+';
+
+echo '
+<url>
+<loc>'; echo "https://www.canakkale-webyazilim.com.tr/blog/"; echo 'mikroservis-mimarisi-faydalar-ve-zorluklar</loc>
+<lastmod>'; echo date("m"); echo '</lastmod>
+<changefreq>weekly</changefreq>
+<priority>1.0</priority>
+</url>
+';
+
+echo '
+<url>
+<loc>'; echo "https://www.canakkale-webyazilim.com.tr/blog/"; echo 'mobil-uygulama-gelistirme-native-vs-cross-platform</loc>
+<lastmod>'; echo date("m"); echo '</lastmod>
+<changefreq>weekly</changefreq>
+<priority>1.0</priority>
+</url>
+';
+
+echo '
+<url>
+<loc>'; echo "https://www.canakkale-webyazilim.com.tr/blog/"; echo 'modern-uygulamalar-icin-docker-konteynerizasyonu</loc>
+<lastmod>'; echo date("m"); echo '</lastmod>
+<changefreq>weekly</changefreq>
+<priority>1.0</priority>
+</url>
+';
+
+echo '
+<url>
+<loc>'; echo "https://www.canakkale-webyazilim.com.tr/blog/"; echo 'modern-web-siteleri-icin-mobil-oncelikli-tasarim-prensipleri</loc>
+<lastmod>'; echo date("m"); echo '</lastmod>
+<changefreq>weekly</changefreq>
+<priority>1.0</priority>
+</url>
+';
+
+echo '
+<url>
+<loc>'; echo "https://www.canakkale-webyazilim.com.tr/blog/"; echo 'python-django-olceklenebilir-web-uygulamalari-olusturma</loc>
+<lastmod>'; echo date("m"); echo '</lastmod>
+<changefreq>weekly</changefreq>
+<priority>1.0</priority>
+</url>
+';
+
+echo '
+<url>
+<loc>'; echo "https://www.canakkale-webyazilim.com.tr/blog/"; echo 'react-vs-angular-dogru-frontend-frameworku-secmek</loc>
+<lastmod>'; echo date("m"); echo '</lastmod>
+<changefreq>weekly</changefreq>
+<priority>1.0</priority>
+</url>
+';
+
+echo '
+<url>
+<loc>'; echo "https://www.canakkale-webyazilim.com.tr/blog/"; echo 'turk-isletmeleri-icin-ozel-crm-sistemlerinin-faydalari</loc>
+<lastmod>'; echo date("m"); echo '</lastmod>
+<changefreq>weekly</changefreq>
+<priority>1.0</priority>
+</url>
+';
+
+echo '
+<url>
+<loc>'; echo "https://www.canakkale-webyazilim.com.tr/blog/"; echo 'verimli-dagitim-icin-devops-ve-ci-cd-pipelinelari</loc>
+<lastmod>'; echo date("m"); echo '</lastmod>
+<changefreq>weekly</changefreq>
+<priority>1.0</priority>
+</url>
+';
+
+echo '
+<url>
+<loc>'; echo "https://www.canakkale-webyazilim.com.tr/blog/"; echo 'web-erisebilirligi-wcag-yonergelerini-uygulama</loc>
+<lastmod>'; echo date("m"); echo '</lastmod>
+<changefreq>weekly</changefreq>
+<priority>1.0</priority>
+</url>
+';
+
+echo '
+<url>
+<loc>'; echo "https://www.canakkale-webyazilim.com.tr/blog/"; echo 'web-sitesi-performans-optimizasyon-teknikleri</loc>
+<lastmod>'; echo date("m"); echo '</lastmod>
+<changefreq>weekly</changefreq>
+<priority>1.0</priority>
+</url>
+';
+
+echo '
+<url>
+<loc>'; echo "https://www.canakkale-webyazilim.com.tr/blog/"; echo 'web-uygulamalari-icin-api-guvenligi-en-iyi-uygulamalari</loc>
+<lastmod>'; echo date("m"); echo '</lastmod>
+<changefreq>weekly</changefreq>
+<priority>1.0</priority>
+</url>
+';
+
+echo '
+<url>
+<loc>'; echo "https://www.canakkale-webyazilim.com.tr/blog/"; echo 'web-uygulamalari-icin-siber-guvenlik-en-iyi-uygulamalari</loc>
+<lastmod>'; echo date("m"); echo '</lastmod>
+<changefreq>weekly</changefreq>
+<priority>1.0</priority>
+</url>
+';
+
+echo '
+<url>
+<loc>'; echo "https://www.canakkale-webyazilim.com.tr/blog/"; echo 'yuksek-performansli-uygulamalar-icin-veritabani-optimizasyon-teknikleri</loc>
 <lastmod>'; echo date("m"); echo '</lastmod>
 <changefreq>weekly</changefreq>
 <priority>1.0</priority>
