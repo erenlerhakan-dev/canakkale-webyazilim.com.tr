4fa44412f02836a1bc47ffa7a6fcbd8aba5336f9
hakan <hakan@hakan>
2026-03-18 16:04:19 +0300

    start 2026

--- en/header.php
+++ en/header.php
@@ -6,7 +6,7 @@
 <div class="header position-relative">
 
 <div class="header__logo top-logo">
-<a href="/en/index">
+<a href="/en/">
 <h1 style="font-size: 18px;font-weight: 800;color: #555555;text-align: center;">Çanakkale Web Development</h1>
 </a>
 </div>
