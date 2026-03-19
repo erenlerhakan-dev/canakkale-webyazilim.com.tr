ee1319c9de9fc06512158a7e0b44b4a41cc4188c
hakan <hakan@hakan>
2026-03-19 19:27:52 +0300

    start 2026

--- footer_scripts.php
+++ footer_scripts.php
@@ -17,6 +17,5 @@
 <script src="/assets/js/plugins/animation-text.min.js"></script>
 <script src="/assets/js/plugins/vivus.min.js"></script>
 <script src="/assets/js/plugins/some-plugins.js"></script>
-<script src="/assets/js/plugins/jquery.validate.min.js"></script>
 <script src="/assets/js/main.js"></script>
 <script src="/assets/js/jquery.nice-select.min.js"></script>
